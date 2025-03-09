a4ab7b8eb3742cef9ed16b909ac30ccc3f6169fe
xglobaltents <xglobaltents@gmail.com>
2025-03-09 20:56:14 +0400

    side menu

--- storefront/src/modules/layout/components/side-menu/index.tsx
+++ storefront/src/modules/layout/components/side-menu/index.tsx
@@ -45,12 +45,12 @@ const SideMenu = ({ regions }: { regions: HttpTypes.StoreRegion[] | null }) => {
                 leaveFrom="opacity-100 backdrop-blur-2xl"
                 leaveTo="opacity-0"
               >
-                <Popover.Panel className="flex flex-col absolute w-full pr-4 sm:pr-0 sm:w-1/3 2xl:w-1/4 sm:min-w-min h-[calc(100vh-1rem)] z-30 inset-x-0 text-sm text-ui-fg-on-color m-2 backdrop-blur-2xl">
+                <Popover.Panel className="flex flex-col absolute w-full sm:pr-0 sm:w-1/3 2xl:w-1/4 sm:min-w-min h-[calc(100vh-1rem)] z-50 inset-x-0 text-sm text-ui-fg-on-color backdrop-blur-2xl">
                   <div
                     data-testid="nav-menu-popup"
-                    className="flex flex-col h-full bg-[rgba(3,7,18,0.5)] rounded-rounded justify-between p-6"
+                    className="flex flex-col h-5/6 bg-[rgba(3,7,18,0.5)] rounded-rounded justify-between p-6"
                   >
-                    <div className="flex justify-end" id="xmark">
+                    <div className="flex justify-center" id="xmark">
                       <button data-testid="close-menu-button" onClick={close}>
                         <XMark />
                       </button>
@@ -71,6 +71,12 @@ const SideMenu = ({ regions }: { regions: HttpTypes.StoreRegion[] | null }) => {
                         )
                       })}
                     </ul>
+                    <div className="scrollHorizantal">
+                    <a href="/categories/shop-by-brands">Shop by brand</a>
+                    <a href="/categories/shop-by-section">Shop by section</a>
+                    <a href="/collections/sales">Sale Collection</a>
+                    <a href="/collections/best-seller">Best seller</a>
+                    </div>
                     <div className="flex flex-col gap-y-6">
                       <div
                         className="flex justify-between"
@@ -91,7 +97,7 @@ const SideMenu = ({ regions }: { regions: HttpTypes.StoreRegion[] | null }) => {
                         />
                       </div>
                       <Text className="flex justify-between txt-compact-small">
-                        © {new Date().getFullYear()} xGlobal Tents. All rights
+                        © {new Date().getFullYear()} Makeup Store.<br></br> All rights
                         reserved.
                       </Text>
                     </div>
