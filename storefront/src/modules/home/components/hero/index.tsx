e5cf9f206137ee7120204218557823867140e1a4
xglobaltents <xglobaltents@gmail.com>
2025-03-10 11:48:40 +0400

    home page region

--- storefront/src/modules/home/components/hero/index.tsx
+++ storefront/src/modules/home/components/hero/index.tsx
@@ -1,7 +1,11 @@
 import { Github } from "@medusajs/icons"
 import { Button, Heading } from "@medusajs/ui"
 
-const Hero = () => {
+interface HeroProps {
+  storeUrl: string;
+}
+
+const Hero: React.FC<HeroProps> = ({ storeUrl }) => {
   return (
     <div className="w-full h-full relative  border-b border-ui-border-base bg-gradient-to-r from-blue-200 to-blue-300">
       <div className="relative inset-0 z-10 flex flex-col justify-center items-center text-center small:p-32 gap-5">
@@ -114,7 +118,7 @@ m429 -168 c94 -39 145 -121 152 -243 8 -145 -32 -228 -134 -278 -48 -24 -72
           </Heading>
         </span>
         <a
-          href="/store"
+          href={storeUrl}
         >
           <button className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded-2xl shadow">
               Tent Store
