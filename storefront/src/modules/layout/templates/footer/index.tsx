77464961254937b565a1d76c24f203618048fcc3
xglobaltents <xglobaltents@gmail.com>
2025-03-10 10:15:10 +0400

    ++

--- storefront/src/modules/layout/templates/footer/index.tsx
+++ storefront/src/modules/layout/templates/footer/index.tsx
@@ -145,7 +145,7 @@ export default async function Footer() {
         </div>
         <div className="flex w-full mb-16 justify-between text-ui-fg-muted">
           <Text className="txt-compact-small">
-            © {new Date().getFullYear()} xGlobal Tents. All rights reserved.
+            © {new Date().getFullYear()} xGlobal Tents.<br></br> All rights reserved.
           </Text>
           <MedusaCTA />
         </div>
