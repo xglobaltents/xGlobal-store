0a315ea4b26ec28cb4f81494d0d9dce0579e5b61
xglobaltents <xglobaltents@gmail.com>
2025-03-12 23:48:48 +0400

    coutries seo

--- storefront/src/app/[countryCode]/(main)/page.tsx
+++ storefront/src/app/[countryCode]/(main)/page.tsx
@@ -4,19 +4,46 @@ import Hero from "@modules/home/components/hero"
 import { getCollectionsWithProducts } from "@lib/data/collections"
 import { getRegion } from "@lib/data/regions"
 
+// Map country codes to country names
+const countryNames: { [key: string]: string } = {
+  AO: "Angola",
+  AT: "Austria",
+  BH: "Bahrain",
+  BW: "Botswana",
+  TD: "Chad",
+  CG: "Congo",
+  GQ: "Equatorial Guinea",
+  FR: "France",
+  GA: "Gabon",
+  DE: "Germany",
+  IQ: "Iraq",
+  KW: "Kuwait",
+  NL: "Netherlands",
+  NG: "Nigeria",
+  OM: "Oman",
+  QA: "Qatar",
+  RW: "Rwanda",
+  SA: "Saudi Arabia",
+  ZA: "South Africa",
+ //SS: "South Sudan",
+  SE: "Sweden",
+  AE: "United Arab Emirates",
+}
+
 export async function generateMetadata({ params: { countryCode } }): Promise<Metadata> {
   const region = await getRegion(countryCode)
+  const countryName = countryNames[countryCode] || region?.name || countryCode
 
   if (!region) {
     return {
-      title: "xGlobal Tents Supplier and Manufacturer",
+      title: `xGlobal Tents Supplier and Manufacturer in ${countryName}`,
       description: "The Leading Tent Manufacturer, Tents Including Aluminum Tents, Modular Tent Structures",
     }
   }
 
   return {
-    title: `xGlobal Tents Supplier and Manufacturer in ${region.name}`,
-    description: `The Leading Tent Manufacturer in ${region.name}, Tents Including Aluminum Tents, Modular Tent Structures`,
+    title: `xGlobal Tents Supplier and Manufacturer in ${countryName}`,
+    description: `The Leading Tent Manufacturer in ${countryName}, Tents Including Aluminum Tents, Modular Tent Structures`,
   }
 }
 
