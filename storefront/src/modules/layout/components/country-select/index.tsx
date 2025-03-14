91f7072edee86b61e5ec1b59f0e3209cd09b24a4
xglobaltents <xglobaltents@gmail.com>
2025-03-14 09:11:59 +0400

    ++

--- storefront/src/modules/layout/components/country-select/index.tsx
+++ storefront/src/modules/layout/components/country-select/index.tsx
@@ -94,7 +94,7 @@ const CountrySelect = ({ toggleState, regions }: CountrySelectProps) => {
             leaveTo="opacity-0"
           >
             <Listbox.Options
-              className="absolute -bottom-[calc(100%-36px)] left-0 xsmall:left-auto xsmall:right-0 max-h-[442px] overflow-y-scroll z-[900] bg-white drop-shadow-md text-small-regular uppercase text-black no-scrollbar rounded-rounded w-full"
+              className="absolute -bottom-[calc(100%-36px)] left-0 xsmall:left-auto xsmall:right-0 max-h-[442px] overflow-y-scroll z-[900] bg-white  text-small-regular uppercase text-black no-scrollbar rounded-rounded w-full"
               static
             >
               {options?.map((o, index) => {
